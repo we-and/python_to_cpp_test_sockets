aaf58ead4dbe81e1bfef30fe5fc366b51dcb03d0
JD <jbmdumont@gmail.com>
2024-05-24 20:04:07 +0100

    Update server_custom.hpp

--- src/pos_server/logic/server_custom.hpp
+++ src/pos_server/logic/server_custom.hpp
@@ -3,7 +3,7 @@
 #include <chrono>
 #include <iomanip>
 #include <string>
-#include <stdlib.h>  
+#include <stdlib.h>
 #include <sstream>
 #include <curl/curl.h>
 #include <filesystem>
@@ -24,165 +24,104 @@
 #include <cryptopp/sha.h>
 #include <cryptopp/hex.h>
 #include <cstdlib>
-#include <sys/stat.h> 
+#include <sys/stat.h>
 #include "setup.hpp"
 
-void processNewData(char* newData, size_t newDataLength,Logger * logger) {
+
+struct ClientState {
+    int socket;
+    std::string buffer;
+
+    ClientState(int sock) : socket(sock) {}
+};
+
+void processNewData(char *newData, size_t newDataLength, Logger *logger)
+{
     // Process the data starting at 'newData' and having length 'newDataLength'
     // For example, this could parse messages, log data, etc.
-    std::string receivedData(newData, newDataLength);  // Construct string from new data
+    std::string receivedData(newData, newDataLength); // Construct string from new data
     logger->log("Received new data: ");
     logger->log(receivedData);
     // Further processing...
 }
 
-void handleClientCustom(int new_socket, struct sockaddr_in address, const std::string& sessionToken, const Config& appConfig, Logger* logger) {
+void handleClientCustom(int new_socket, struct sockaddr_in address, const std::string &sessionToken, const Config &appConfig, Logger *logger)
+{
     std::cout << "Connection from " << inet_ntoa(address.sin_addr) << " established." << std::endl;
-    logger->log("Connection from " + std::string(inet_ntoa(address.sin_addr)) + " established.");  // Log connection
+    logger->log("Connection from " + std::string(inet_ntoa(address.sin_addr)) + " established."); // Log connection
+  fd_set read_fds;
+    std::vector<char> buffer(1024);
+    std::string message_buffer;
 
-    int bufferSize=appConfig.bufferSize;
-    std::vector<char> buffer(bufferSize); // Start with an initial size
-    int totalBytesRead = 0;
-    int bytesRead = 0;
-
-    std::string dataBuffer; 
+    // Set socket to non-blocking mode
+    fcntl(new_socket, F_SETFL, O_NONBLOCK);
 
     while (true) {
-        fd_set readfds;
-        FD_ZERO(&readfds);
-        FD_SET(new_socket, &readfds);
+        FD_ZERO(&read_fds);
+        FD_SET(new_socket, &read_fds);
+        struct timeval timeout = {1, 0}; // 1 second timeout
 
-        struct timeval timeout;
-        timeout.tv_sec = 5;
-        timeout.tv_usec = 0;
+        int activity = select(new_socket + 1, &read_fds, NULL, NULL, &timeout);
 
-        int activity = select(new_socket + 1, &readfds, NULL, NULL, &timeout);
-        if ((activity < 0) && (errno != EINTR)) {
-            std::cerr << "Select error" << std::endl;
+        if (activity < 0) {
+            logger->log("Select error");
+            break;
         }
 
-        if (activity > 0 && FD_ISSET(new_socket, &readfds)) {
-            bytesRead = read(new_socket, buffer.data() + totalBytesRead, buffer.size() - totalBytesRead);
-            if (bytesRead > 0) {
-                logger->log("---------------------------------------------");
-                 processNewData(buffer.data() + totalBytesRead, bytesRead,logger);
+        if (FD_ISSET(new_socket, &read_fds)) {
+            int bytes_read = read(new_socket, buffer.data(), buffer.size());
+            if (bytes_read > 0) {
+                message_buffer.append(buffer.data(), bytes_read);
 
-                 logger->log(std::to_string(bytesRead));  // Log connection
-                 // Log connection
-                logger->log("data");
-                logger->log("wasEmpty");
+                size_t start_pos = message_buffer.find("<isomsg>");
+                size_t end_pos = message_buffer.find("</isomsg>");
 
-                bool wasEmpty=(dataBuffer.empty()) ;
-                logger->log(wasEmpty?1:0);
-                logger->log("buffer");
+                while (start_pos != std::string::npos && end_pos != std::string::npos) {
+                    std::string complete_message = message_buffer.substr(start_pos, end_pos - start_pos + 9);
+                    processmessage(complete_message);
 
-                logger->log( buffer.data());
-                 logger->log("dataBuffer before");
-
-                logger->log( dataBuffer.data());
-                dataBuffer.append(buffer.data(), bytesRead);
-                 logger->log("dataBuffer after");
-
-                logger->log( dataBuffer.data());
-
-                totalBytesRead += bytesRead;
-                logger->log( std::to_string(totalBytesRead));
-                // Resize buffer if needed
-                if (totalBytesRead == buffer.size()) {
-                    logger->log("resize buffer");  // Log connection
-                    buffer.resize(buffer.size() + bufferSize);
+                    message_buffer.erase(0, end_pos + 9);
+                    start_pos = message_buffer.find("<isomsg>");
+                    end_pos = message_buffer.find("</isomsg>");
                 }
 
-                std::string d(buffer.begin(), buffer.end());
-
-   // Check if the buffer starts with <isomsg>
-            if (d.find("<isomsg>") != 0) {
-                 logger->log("not starting with <isomsg>,erase buffer");  // Log received data
-                        
-                    resendToRequestor(new_socket, d);
-                    d.clear();
-            }
-
-            //if (wasEmpty){
-             
-            // Check if the accumulated data starts with <isomsg>
-            if (  d.find("<isomsg>") == 0) {
-                                  logger->log("Data buffer starts with <isomsg>");
-            
-                size_t start, end;
-                // Keep processing while complete messages are in the buffer
-                while ((start = d.find("<isomsg>")) != std::string::npos &&
-                    (end = d.find("</isomsg>", start)) != std::string::npos) {
-
-                                        logger->log("msg is complete");  // Log connection
-
-                    std::string data = d.substr(start, end + 9 - start); // 9 is length of "</isomsg>"
-
-                        logger->log("Received data from client: " + data);  // Log received data
-                        std::string cleanpayload=removeNewLines(data);
-                            logger->log("cleanpayload"+cleanpayload);
-                        
-                        // Process the data...
-                        // If data was received, echo it back to the client
-                        if (isISO8583(cleanpayload)) {
-                            std::string payload = cleanpayload;
-                            logger->log("valid isomsg");
-                            checkTokenAndExecute(new_socket, sessionToken, payload, appConfig);
-                        } else {
-                            logger->log("Not a valid isomsg");
-                            // Reject if not valid
-                            resendToRequestor(new_socket, data);
-                        }
-
-                  logger->log("erase buffer");  // Log received data
-                    d.erase(start, end + 9 - start);
+                if (!message_buffer.empty() && message_buffer.find("<isomsg>") != 0) {
+                    resend(message_buffer);
+                    message_buffer.clear();
                 }
-
-            }/*else{
-             
-                if (wasEmpty){
-                    logger->log("not starting with <isomsg>,erase buffer");  // Log received data
-                        
-                    resendToRequestor(new_socket, dataBuffer);
-                    dataBuffer.clear();
-                }
-           // }
-
-            }*/
-
-
-            } else if (bytesRead == 0) {
-                std::cout << "Client disconnected" << std::endl;
-                logger->log("Client disconnected"); 
+            } else if (bytes_read == 0) {
+                // Connection closed by client
                 break;
-            } else {
-                logger->log("read failed"); 
-                perror("read failed");
+            } else if (errno != EWOULDBLOCK && errno != EAGAIN) {
+                logger->log("Read error");
                 break;
             }
         }
-
     }
 
     close(new_socket);
+
 }
-void startServerCustom(std::string sessionToken,const Config& appConfig){
-     Logger* logger = Logger::getInstance();
+void startServerCustom(std::string sessionToken, const Config &appConfig)
+{
+    Logger *logger = Logger::getInstance();
     logger->log("StartServer");
     int server_fd, new_socket;
     struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
-    int port = appConfig.port;  // Port number on which the server will listen for connections
-   
+    int port = appConfig.port; // Port number on which the server will listen for connections
+
     // Creating socket file descriptor
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
         perror("socket failed");
         exit(EXIT_FAILURE);
     }
 
-    // Forcefully attaching socket to the port 
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+    // Forcefully attaching socket to the port
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+    {
         perror("setsockopt");
         close(server_fd);
         exit(EXIT_FAILURE);
@@ -191,21 +130,24 @@ void startServerCustom(std::string sessionToken,const Config& appConfig){
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(port);
 
-    // Forcefully attaching socket to the port 
-    if (bind(server_fd, (struct sockaddr*)&address, sizeof(address)) < 0) {
+    // Forcefully attaching socket to the port
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
         perror("bind failed");
         close(server_fd);
         exit(EXIT_FAILURE);
     }
-    if (listen(server_fd, 3) < 0) {
+    if (listen(server_fd, 3) < 0)
+    {
         perror("listen");
         close(server_fd);
         exit(EXIT_FAILURE);
     }
 
-   
-    while (true) {
-        if ((new_socket = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)) < 0) {
+    while (true)
+    {
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+        {
             perror("accept");
             close(server_fd);
             exit(EXIT_FAILURE);
