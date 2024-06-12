f1577c533a21945468a8769af345500c4726f0e3
JD <jbmdumont@gmail.com>
2024-06-12 13:57:40 +0300

    Update log_utils.hpp

--- src/pos_server/utils/log_utils.hpp
+++ src/pos_server/utils/log_utils.hpp
@@ -7,10 +7,6 @@
 #include <iomanip>
 #include <string>
 
-#include <ctime>
-#include <string>
-
-
 #include <mutex>
 #include <stdlib.h>  
 #include <sstream>
@@ -37,10 +33,8 @@ class Logger {
 private:
     static Logger* instance;
     static std::mutex mutex;
-     std::chrono::system_clock::time_point logStartTime;
     Config appConfig;  // Configuration instance as a class member
-    std::string initialDate;
-  int initialWeekNumber;
+
 protected:
     Logger() {}  // Constructor is protected
 
@@ -56,68 +50,22 @@ public:
         }
         return instance;
     }
-    bool shouldRotateLogFile() {
-        auto now = std::chrono::system_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::hours>(now - logStartTime).count();
-        return duration >= 168;  // 7 days * 24 hours = 168 hours
-    }
-
-    void rotateLogFile() {
-     //   this->log(" > rotateLogFile");
-        logStartTime = std::chrono::system_clock::now();
-        this->log("initialDate");
-        this->log(initialDate);
-        this->log(initialWeekNumber);
-
-    }
-      std::string getFilePath() {
-        auto now = std::chrono::system_clock::now();
-        auto tt = std::chrono::system_clock::to_time_t(now);
-    
-        // Get the current week number
-        std::tm now_tm = *std::localtime(&tt);
-        char weekNumberStr[3]; // Week number can be two digits
-        std::strftime(weekNumberStr, sizeof(weekNumberStr), "%U", &now_tm);
-        int currentWeekNumber = std::stoi(weekNumberStr);
-
-        return appConfig.logsDir + "/log.txt";
-//        return appConfig.logsDir + "/log-" + initialDate + "-" + std::to_string(initialWeekNumber + (currentWeekNumber - initialWeekNumber)) + ".txt";
-  
-    }
-
+std::string getFilePath(const Config& appConfig){
+      auto filePath=appConfig.logsDir + "/log-" + std::to_string(t) + ".txt";
+      return filePath;
+}
     // Initialization method for setting up the configuration
     void init(const Config& appConfig_) {
         appConfig = appConfig_;
-          auto filePath=getFilePath();
-           logStartTime = std::chrono::system_clock::now();
-
-            // Get the initial date and week number
-        auto tt = std::chrono::system_clock::to_time_t(logStartTime);
-        auto now_tm = *std::localtime(&tt);
-
-        char dateStr[11]; // Date in YYYY-MM-DD format
-        std::strftime(dateStr, sizeof(dateStr), "%Y-%m-%d", &now_tm);
-        initialDate = dateStr;
-
-
-
-        char weekNumberStr[3]; // Week number can be two digits
-        std::strftime(weekNumberStr, sizeof(weekNumberStr), "%U", &now_tm);
-        initialWeekNumber = std::stoi(weekNumberStr);
-
-          rotateLogFile();
-
+          auto filePath=getFilePath(appConfig_);
         std::cout <<"Log file               :"<< filePath <<std::endl;
+
     }
     void log(const int& text) {
         return log(std::to_string(text));
     }
 
     void log(const std::string& text) {
-          if (shouldRotateLogFile()) {
-            rotateLogFile();
-        }
-
         // Retrieve the current system time as a time_t object
         auto now = std::chrono::system_clock::now();
         auto tt = std::chrono::system_clock::to_time_t(now);
@@ -136,4 +84,34 @@ public:
         log_file << std::put_time(std::localtime(&tt), "%F %T") << " - " << text << "\n";
     }
 };
+
+/**
+ * Logs a message to a uniquely named file with a timestamp.
+ * 
+ * This function takes a string message and logs it to a text file. The filename is uniquely generated based on the current system time.
+ * Each log entry in the file is prefixed with a timestamp in the "YYYY-MM-DD HH:MM:SS" format, followed by the message itself.
+ * 
+ * @param text The message to be logged. It should be a string containing the text that needs to be recorded in the log file.
+ * 
+ * Usage Example:
+ * log_to_file("Application has started.");
+ * 
+ * Output File Example: log-1700819387.txt
+ * Contents of log-1700819387.txt: 2024-04-30 12:34:56 - Application has started.
+
+using json = nlohmann::json;
+void log_to_file(const std::string& text) {
+
+    // Create or open a log file named with the current time stamp
+    std::ofstream log_file("log-" + std::to_string(t) + ".txt", std::ios::app);
+
+
+    // Retrieve the current system time as a time_t object
+    auto tt = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+
+    // Write the current time and the log message to the file
+    log_file << std::put_time(std::localtime(&tt), "%F %T") << " - " << text << "\n";
+}
+*/
+
 #endif
